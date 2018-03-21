3b4a47030b3e0598e618d9da03613b1fd7a8f94c
silveiratm <37445977+silveiratm@users.noreply.github.com>
2018-03-21 13:43:56 -0300

    Update webserver.py

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -20,8 +20,8 @@ class webServerHandler(BaseHTTPRequestHandler):
             if self.path.endswith("/edit"):
                 restaurantIDPath = self.path.split("/")[2]
                 myRestaurantQuery = session.query(Restaurant).filter_by(
-                    id =restaurantIDPath).one()
-                if myRestaurantQuery != []:
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
@@ -30,7 +30,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += myRestaurantQuery.name
                     output += "</h1>"
                     output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/edit' >" % restaurantIDPath
-                    output += "<input name= 'newRestaurantName' type='text' placegolder = '%s' >" % myRestaurantQuery.name
+                    output += "<input name= 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
                     output += "<input type= 'submit' value='Rename'>"
                     output += "</form></body></html>"
                     

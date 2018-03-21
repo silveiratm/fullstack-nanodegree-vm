35eb5c3d84b939c941290805f553bed02dcf111b
silveiratm <37445977+silveiratm@users.noreply.github.com>
2018-03-21 11:48:08 -0300

    Update webserver.py

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -17,6 +17,21 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
+            
+             if self.path.endswith("/restaurants/new"):
+                output = ""
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output += "<html><body>"
+                output += " <h2>Make a New Restaurant</h2>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                output += "<input name='newRestaurantName' type='text' placeholder = 'New Restaurant Name' >" 
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+            
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
@@ -24,6 +39,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output += "<html><body>"
+                output += " <a href='/restaurants/new'>Make a New Restaurant Here</a></br></br>"
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "</br>"
@@ -35,18 +51,38 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
-            
+                       
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    
+                newRestaurant = Restaurant(name=messagecontent[0])
+                session.add(newRestaurant)
+                session.commit()
+                
+                self.response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+        except:
+            pass
     
 def main():
     try:
         server = HTTPServer(('', 8080), webServerHandler)
-        print 'Web Server running...open localhost:8080/restaurants in you browser'
+        print 'Web Server running... Open localhost:8080/restaurants in you browser'
         server.serve_forever()
     except KeyboardInterrupt:
-        print  '^C receibed, shutting down server'
+        print  '^C received, shutting down server'
         server.socket.close()
 
 if __name__ == '__main__':
